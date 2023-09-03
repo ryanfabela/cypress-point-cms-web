57d11aa7a1fe691153438c39975f843ce0ea4e10
Ryan Fabela <01somebody@gmail.com>
2023-09-03 18:01:47 -0500

    added button component

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,7 +1,12 @@
-import type {Meta, StoryObj} from "@storybook/react";
-import {Button} from "./Button";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
 
-const meta: Meta<typeof Button> = {component: Button};
+const meta: Meta<typeof Button> = { component: Button };
 export default meta;
 type Story = StoryObj<typeof Button>;
-export const Primary: Story = {args: {}};
+export const Primary: Story = {
+  args: {
+    children: "Button",
+    variant: "primary",
+  },
+};
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,7 +1,24 @@
-import React from "react";
-import "./Button.scss";
+import React, { ButtonHTMLAttributes } from "react";
+import clsx from "clsx";
+import styles from "./Button.module.scss";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "small" | "medium" | "large";
+  variant?: "primary" | "secondary";
+  children?: React.ReactNode;
 }
 
-export const Button = (): ButtonProps => <div>Button_component</div>;
+export const Button = ({
+  children,
+  className,
+  variant,
+  size,
+  ...rest
+}: ButtonProps): JSX.Element => (
+  <button
+    className={clsx(styles.button, styles[variant], styles[size], className)}
+    {...rest}
+  >
+    {children}
+  </button>
+);
