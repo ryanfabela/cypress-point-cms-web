641db68dcf66651681aef0bcdaff3f2aa263ff26
Ryan Fabela <01somebody@gmail.com>
2023-10-05 20:50:27 -0500

    added variants and basic styling for the card component

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-import "./Card.scss";
+import styles from "./Card.module.scss";
+import clsx from "clsx";
 
 export interface CardProps {
+  children: React.ReactNode;
+  variant?:
+    | "black"
+    | "white"
+    | "gray"
+    | "lightPrimary"
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "gray";
 }
 
-export const Card = (): CardProps => <div className="card">Card_component</div>;
+export const Card = ({ children, variant = "white" }: CardProps) => (
+  <div className={clsx(styles.card, styles[variant])}>{children}</div>
+);
